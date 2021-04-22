573ca3af1e1157ae6bf27b099cb161f3f8aa84f2
zhoujumbo <2505148618@qq.com>
2021-04-22 20:28:39 +0800

    增加Wrapper的单元测试

---
+++ src/test/java/org/apache/ibatis/reflection/wrapper/WrapperTest.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.reflection.wrapper;
+
+import org.apache.ibatis.domain.misc.CustomBeanWrapperFactory;
+import org.apache.ibatis.domain.misc.RichType;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+/**
+ * 自己创建的测试类
+ * 调试 Wrapper
+ */
+public class WrapperTest {
+
+  @Test
+  void shouldTestBeanWrapperGetGetterType() {
+
+    RichType object = new RichType();
+
+    if (true) {
+      object.setRichType(new RichType());
+      object.getRichType().setRichMap(new HashMap());
+      object.getRichType().getRichMap().put("nihao", "123");
+    }
+
+    MetaObject meta = MetaObject.forObject(object, SystemMetaObject.DEFAULT_OBJECT_FACTORY, new CustomBeanWrapperFactory(), new DefaultReflectorFactory());
+    Class<?> clazz = meta.getObjectWrapper().getGetterType("richType.richMap.nihao");
+    System.out.println(clazz);
+  }
+}
